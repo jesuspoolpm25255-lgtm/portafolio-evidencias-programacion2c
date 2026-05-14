6464a12a79862a7bbbcf69de70e06c9c709e2a7e
Jesus Pool <jesus.pool_pm25255@cecyre.edu.mx>
2026-05-14 11:38:09 -0600

    Se entrega los ejercicios del parcial3

---
+++ ejercicios/jesus-pool/parcial2/parcial3/testing2.py
@@ -0,0 +1,14 @@
+def suma (a,b):
+    return a + b
+
+import unittest
+
+class TestSuma(unittest.TestCase):
+    def test_suma_positiva(self):
+        self.assertEqual(suma(2,3), 5)
+
+    def test_suma_negativa(self):
+        self.assertEqual(suma(-2, -3), -5)
+
+        def test_suma_mixta(self):
+            self.aseert
